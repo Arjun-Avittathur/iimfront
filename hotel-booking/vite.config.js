01d4c0892462fc57395bcbb143b3e98330487638
Arjun Avittathur <arjun.avittathur@mail.com>
2025-05-22 12:42:50 +0530

    r

--- hotel-booking/vite.config.js
+++ hotel-booking/vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Put all node_modules packages into a separate chunk named 'vendor'
-          if (id.includes('node_modules')) {
-            return 'vendor'
-          }
-        }
-      }
-    }
-  }
+  base: process.env.VITE_BASE_PATH || "/iimfront ",
 })
